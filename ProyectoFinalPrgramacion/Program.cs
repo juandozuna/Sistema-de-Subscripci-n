7eb0acd5b00dc1fe75a808b072ad65f1106b374d
juandozuna <juandanielozuna2@gmail.com>
2017-08-06 17:10:07 -0400

    Probando

--- ProyectoFinalPrgramacion/Program.cs
+++ ProyectoFinalPrgramacion/Program.cs
@@ -10,6 +10,8 @@ namespace ProyectoFinalPrgramacion
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
         }
     }
 }
