8da509b57ff38b8d7240c9588d0f364380ce054e
David De Los Santos <ddelossantosmateo@gmail.com>
2017-09-21 18:25:34 -0400

    Creando API de servicios

---
+++ MVCUpdate/MVCSuscriptionSystem/Controllers/ServiciosController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using MVCSuscriptionSystem.Models;
+
+namespace MVCSuscriptionSystem.Controllers
+{
+    public class ServiciosController : ApiController
+    {
+        private EntityModel db = new EntityModel();
+
+        // GET: api/Servicios
+        public IQueryable<Servicio> GetServicios()
+        {
+            return db.Servicios;
+        }
+
+        // GET: api/Servicios/5
+        [ResponseType(typeof(Servicio))]
+        public IHttpActionResult GetServicio(int id)
+        {
+            Servicio servicio = db.Servicios.Find(id);
+            if (servicio == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(servicio);
+        }
+
+        // PUT: api/Servicios/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutServicio(int id, Servicio servicio)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != servicio.ServicioID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(servicio).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ServicioExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Servicios
+        [ResponseType(typeof(Servicio))]
+        public IHttpActionResult PostServicio(Servicio servicio)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Servicios.Add(servicio);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = servicio.ServicioID }, servicio);
+        }
+
+        // DELETE: api/Servicios/5
+        [ResponseType(typeof(Servicio))]
+        public IHttpActionResult DeleteServicio(int id)
+        {
+            Servicio servicio = db.Servicios.Find(id);
+            if (servicio == null)
+            {
+                return NotFound();
+            }
+
+            db.Servicios.Remove(servicio);
+            db.SaveChanges();
+
+            return Ok(servicio);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ServicioExists(int id)
+        {
+            return db.Servicios.Count(e => e.ServicioID == id) > 0;
+        }
+    }
+}
