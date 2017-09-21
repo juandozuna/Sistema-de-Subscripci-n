97376ed290edeab8262a67717ed865e92f8b772e
juandozuna <juandanielozuna2@gmail.com>
2017-09-21 16:47:18 -0400

    RolesSeAsignanEnCreaciónDeUsuarios

--- MVCUpdate/MVCSuscriptionSystem/MethodManagers/AdminManager.cs
+++ MVCUpdate/MVCSuscriptionSystem/MethodManagers/AdminManager.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using MVCSuscriptionSystem.Models;
 
 namespace MVCSuscriptionSystem.MethodManagers
@@ -11,7 +13,19 @@ namespace MVCSuscriptionSystem.MethodManagers
     {
         public static void AddRolesToUser(FormCollection collection)
         {
-            
+            using (ApplicationDbContext context = new ApplicationDbContext())
+            {
+                var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+                var user = context.Users.Find(collection["Id"]);
+                Dictionary<string, bool> checkboxes = new Dictionary<string, bool>();
+                var keys = collection.AllKeys;
+                for(int i = 2; i < collection.Count; i++ )
+                {
+                    string role = keys[i];
+                    var result = UserManager.AddToRole(user.Id, role);
+                    
+                }
+            }
         }
     }
 }
