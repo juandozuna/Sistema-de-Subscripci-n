0bc31bd8673c420cbd0b28831dfe35e72cfe58c0
David De Los Santos <ddelossantosmateo@gmail.com>
2017-10-12 18:30:10 -0400

    Pedro unit tests

--- MVCUpdate/MVCSuscriptionSystemTests/ServiciosPedroTests.cs
+++ MVCUpdate/MVCSuscriptionSystemTests/ServiciosPedroTests.cs
@@ -1,14 +1,37 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using MVCSuscriptionSystem.HttpClients.HttpMethods;
+using MVCSuscriptionSystem.HttpClients.WebServicePedro;
+using MVCSuscriptionSystem.Models;
 
 namespace MVCSuscriptionSystemTests
 {
     [TestClass]
     public class UnitTest1
     {
+        public TestContext testContext { get; set; }
+
         [TestMethod]
-        public void TestMethod1()
+        public void GetServiciosPedroTest()
         {
+            ServiciosPedro3 cliente = new ServiciosPedro3();
+            var servicios = cliente.GetServicios();
+            foreach (var item in servicios)
+            {
+                testContext.WriteLine(item.Nombre);
+                testContext.WriteLine(item.Precio.ToString());
+                testContext.WriteLine("#####");
+            }
+            Assert.IsTrue(servicios.Count > 0);
         }
+
+        //[TestMethod]
+        //public void GetServicioPedroEspecifico()
+        //{
+
+        //}
+
+
     }
 }
