31bc8f1d5e707b5d6ee1ba616b9946d2522ca229
David De Los Santos <ddelossantosmateo@gmail.com>
2017-10-12 20:07:32 -0400

    Suscripciones

--- MVCUpdate/MVCSuscriptionSystem/HttpClients/WebServicePedro/SuscripcionesPedro3.cs
+++ MVCUpdate/MVCSuscriptionSystem/HttpClients/WebServicePedro/SuscripcionesPedro3.cs
@@ -3,18 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using MVCSuscriptionSystem.WebReferencePedro;
+using MVCSuscriptionSystem.Models;
 
 namespace MVCSuscriptionSystem.HttpClients.WebServicePedro
 {
     public class SuscripcionesPedro3
     {
 
-        ManejadorServicios s = new ManejadorServicios();
+        ManejadorServicios manejador = new ManejadorServicios();
 
-        public void test(int id, bool estado)
+        public void ActivarDesactivarSuscripcion(int id, bool estado)
         {
 
-            v
+            var buscar = manejador.BuscarIDSuscripcion(id);
+            if (buscar.success)
+            {
+                if (estado)
+                {
+                    manejador.ActivarSuscripcion(id);
+                }
+
+                else
+                {
+                    manejador.DesactivarSuscripcion(id);
+                }
+            }
 
         }
     }
